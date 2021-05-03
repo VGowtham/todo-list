5b3ef299fc43f9e37663633a4604222b467d0549
Gowtham <gowtham.icengr@gmail.com>
2021-05-03 21:07:00 +0530

    User management from admin along with Login & Signup completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: "login",
+    component: LoginComponent
+  },
+  {
     path: 'login',
     component: LoginComponent
   },
